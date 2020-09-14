ccbac19cfea019f8c9bb51a338f589ee73c11231
graytoowolf <graywolf0110@gmail.com>
2020-09-14 16:53:17 +0800

    改动

--- scripts/release.ps1
+++ scripts/release.ps1
@@ -1,6 +1,7 @@
-$manifest = Invoke-WebRequest 'https://dev.azure.com/blessing-skin/51010f6d-9f99-40f1-a262-0a67f788df32/_apis/git/repositories/a9ff8df7-6dc3-4ff8-bb22-4871d3a43936/Items?path=%2Fupdate.json' | ConvertFrom-Json
+$manifest = Invoke-WebRequest 'https://dev.azure.com/graytoowolf/b31f3ddc-31f9-44b4-a216-f4a129ef91e7/_apis/git/repositories/e285d1f1-6f31-4b37-9768-5d7d62b1d80f/items?path=%2Fupdate.json' | ConvertFrom-Json
 $last = $manifest.latest
-$current = (Get-Content package.json | ConvertFrom-Json).version
+$version = (Get-Content package.json | ConvertFrom-Json).version
+$current = (git rev-parse HEAD).Substring(0,7)
 
 # Install dependencies
 composer install --no-dev --prefer-dist --no-suggest --no-progress
@@ -13,21 +14,23 @@ $zip = "blessing-skin-server-$current.zip"
 zip -9 -r $zip app bootstrap config database plugins public resources/lang resources/views resources/misc/textures routes storage vendor .env.example artisan LICENSE README.md README_EN.md index.html
 Write-Host "Zip archive is created." -ForegroundColor Green
 
+
+
 New-Item dist -ItemType Directory
 Set-Location dist
 Copy-Item -Path "../$zip" -Destination $zip
 
-$manifest.latest = $current
+$manifest.latest = $version+'-'+$current
 $manifest.url = $manifest.url.Replace($last, $current)
 $manifest.php = '7.2.5'
 ConvertTo-Json $manifest | Out-File -FilePath update.json
 Write-Host "Update source is prepared." -ForegroundColor Green
 
 $azureToken = $env:AZURE_TOKEN
-git config --global user.email 'g-plane@hotmail.com'
+git config --global user.email 'graytoowolf'
 git config --global user.name 'Pig Fang'
 git init
 git add .
 git commit -m "Publish"
-git push -f "https://anything:$azureToken@dev.azure.com/blessing-skin/Blessing%20Skin%20Server/_git/Blessing%20Skin%20Server" master
+git push -f "https://anything:$azureToken@dev.azure.com/graytoowolf/xz/_git/xz" master
 Write-Host "Update source is pushed to Azure Repos." -ForegroundColor Green
